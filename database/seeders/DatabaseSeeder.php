0a9a99b6f5f5e13dd78da184155e6da07efddaa6
soufianeachgare <soufianeachgare@gmail.com>
2024-11-01 03:17:27 +0100

    Add permission seeder and create super admin with all permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Database\Seeders;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // call the permission seeder
+        $this->call(PermissionTableSeeder::class);
+        // create a super admin
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'lk911game@gmail.com',
+            'password' => bcrypt('123456'),
+            'email_verified_at' => now(),
         ]);
+        // create the super admin role
+        $superAdminRole = Role::create(['name' => 'Super Admin']);
+        // assign all permissions to the super admin role
+        $superAdminRole->givePermissionTo(Permission::all());
+        // assign the super admin role to the super admin
+        $superAdmin->assignRole($superAdminRole);
+        $superAdmin->save();
     }
 }
---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'user-list',
+            'user-create',
+            'user-edit',
+            'user-delete',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
